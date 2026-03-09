df8a4b9a0af1daa31288999ff28f884f5066590f
parasomniya <parasomniya.m@gmail.com>
2026-03-09 19:55:40 +0700

    front and logic added

---
+++ frontend/js/login.js
@@ -0,0 +1,22 @@
+document.getElementById("loginBtn").onclick = async function () {
+
+    const login = document.getElementById("login").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            login: login,
+            password: password
+        })
+    });
+
+    if (response.ok) {
+        window.location.href = "index.html";
+    } else {
+        alert("Неверный логин или пароль");
+    }
+};
