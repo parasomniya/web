c721137888685c22641ca6e206f0fd6032a695f4
Svin52 <yaemtoperov@gmail.com>
2026-05-01 16:10:37 +0700

    Магические константы вынес в отдельный файлик

---
+++ module-3/config.js
@@ -0,0 +1,8 @@
+// config.js
+export const BATCH_START_THRESHOLD_KG = 30;
+export const LEFTOVER_THRESHOLD_KG = 50;
+export const UNLOAD_DROP_THRESHOLD_KG = 200;
+export const UNLOAD_MIN_PEAK_KG = 400;
+export const UNLOAD_UPDATE_DELTA_KG = 1;
+export const UNLOAD_WEIGHT_BUFFER_KG = 50;
+export const EMPTY_VEHICLE_THRESHOLD_KG = 50;
