a8172ea26c1465d54caca71746d72fe3e5e554c3
Svin52 <yaemtoperov@gmail.com>
2026-05-01 17:01:56 +0700

    Провека на аномальные скачки - аномалии просто скипаются

--- module-3/config.js
+++ module-3/config.js
@@ -6,3 +6,4 @@ export const UNLOAD_MIN_PEAK_KG = 400;
 export const UNLOAD_UPDATE_DELTA_KG = 1;
 export const UNLOAD_WEIGHT_BUFFER_KG = 50;
 export const EMPTY_VEHICLE_THRESHOLD_KG = 50;
+export const ANOMALY_THRESHOLD_KG = 200;
